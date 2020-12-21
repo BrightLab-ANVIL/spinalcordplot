aa8d97000325de966180dc87504803762ea20583
khemmerling <khemmerling@u.northwestern.edu>
2020-12-21 13:05:21 -0500

    changes to run file

--- runSCheatmap.m
+++ runSCheatmap.m
@@ -1,6 +1,6 @@
 %% Test example data
 % Add the full path to your output folder from x.heatmapPrep as a string
-% E.g., '~Downloads/spinalcordplot/heatmap_output'
+% E.g., ''~/Downloads/spinalcordplot-main/heatmap_output'
 input_folder=
 
 % Adjust this for tissue type / longitudinal level organization (1 or 0)
@@ -12,6 +12,6 @@ useLevels=1;
 % Don't edit below here ---------------------------------------------------
 TR=2;
 phys_prefix='sub-example';
-phys_loc='exampleData/phys';
-moco_loc='exampleData/motionTraces.txt';
+phys_loc='~/Downloads/spinalcordplot-main/exampleData/phys';
+moco_loc='~/Downloads/spinalcordplot-main/exampleData/motionTraces.txt';
 [heatmap,voxelDir,tstats]=SCheatmap(input_folder,bySlice,useLevels,TR,phys_prefix,phys_loc,moco_loc);
